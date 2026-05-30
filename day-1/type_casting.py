df17fd9d0666f60dfab12cf347229fd0b25d1bc5
iamsiddheshwar <sidddofficial18@gmail.com.com>
2026-05-30 06:30:51 +0000

    new concept

---
+++ day-1/type_casting.py
@@ -0,0 +1,23 @@
+# Type Casting : conversion of one datatype to anotherdatatype is known as type casting
+
+num1 =int (input("enter first number:"))
+      
+      #str in type casting
+
+num2 = int (input("enter second number:"))
+
+sum_of_num= num1 + num2
+
+print("sum of two numbers is :", sum_of_num)
+
+diff_of_num = num1 - num2
+
+print("diff of two numbers is :", diff_of_num)
+
+prod_of_num = num1 * num2
+
+print("prod of two numbers is :", prod_of_num)
+
+div_of_num = num1 / num2
+
+print("div of two numbers is :", div_of_num)
